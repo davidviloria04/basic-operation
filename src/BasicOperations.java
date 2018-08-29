46bf9443b11ede2df8f42bf772b4cebe1b8eff7a
David Viloria <david.viloria0424@gmail.com>
2018-08-28 21:44:07 -0500

    Create basic operations project

---
+++ src/BasicOperations.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class BasicOperations
+{
+    public static void main(String[] args)
+    {
+
+
+        int number1 = Integer.parseInt(JOptionPane.showInputDialog("Enter a Number"));
+
+        int number2 = Integer.parseInt(JOptionPane.showInputDialog("Enter a Number"));
+
+        int suma=number1+number2;
+
+        int resta=number1-number2;
+
+        int division=number1/number2;
+
+        int multiplicacion=number1*number2;
+
+        JOptionPane.showMessageDialog(null, "La suma es: "+suma
+                + "\nLa resta es :"+resta + "\nLa división es :"+division + "\nLa Multiplicación es :"+multiplicacion);
+    }
+}
