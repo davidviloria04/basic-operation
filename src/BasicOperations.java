4aae5b2638bed687238b223ecbe12f29f14801b8
David Viloria <david.viloria0424@gmail.com>
2018-08-28 21:49:48 -0500

    R

--- src/BasicOperations.java
+++ src/BasicOperations.java
@@ -16,7 +16,7 @@ public class BasicOperations
 
         int division=number1 / number2;
 
-        int multiplicacion=number1* number2;
+        int multiplicacion=number1 * number2;
 
         JOptionPane.showMessageDialog(null, "La suma es: "+suma
                 + "\nLa resta es :"+resta + "\nLa división es :"+division + "\nLa Multiplicación es :"+multiplicacion);
